2140f144a545ce7402723985074cc2957f4a85a3
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-01 16:04:59 -0300

    Adicionando variavies

---
+++ javaScript/tipos-dados.js
@@ -0,0 +1,24 @@
+//String
+let nome = "Stefany";
+
+
+
+//Inteiro
+let idade = 26;
+
+//Boleano
+let casado = true;
+
+//Numeros flutuantes com casas decimais
+let valorProduto = 5.35;
+
+//Resultado 
+console.log("Meu nome é " + nome);
+console.log("Minha idade é: ", idade);
+console.log("Casado? ", casado);
+console.log("R$", valorProduto);
+console.log(typeof idade);
+console.log(typeof nome);
+console.log(typeof valorProduto);
+
+
