6590f83a745789da29d0a5ce0d864527255d376d
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-01 15:53:01 -0300

    aprendendo variaveis

---
+++ javaScript/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "olá mundo";
+
+const nomeCompleto = "Stefany Viveiros Barboza";
+let idade = 26;
+
+//Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log( " Meu nome é  " + nomeCompleto + " e eu tenho " + idade + " Anos " );
+
