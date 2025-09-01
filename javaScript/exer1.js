a0de44bf734bceeba0791a23bc543ee91b82a28a
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-01 16:21:30 -0300

    Adicionando variaveis

---
+++ javaScript/exer1.js
@@ -0,0 +1,15 @@
+let nome = "Stefany Barboza";
+let letras = nome[0] + nome[8];
+
+let java = "JavaScript";
+let javaSCript = java[0] + java[4];
+
+//Resultado
+console.log(letras);
+console.log(javaSCript);
+
+
+
+/*javaSCript
+js
+*/
