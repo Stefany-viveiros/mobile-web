35215befc2aa3c10a0681ce3198548efcc129c81
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-03 14:54:09 -0300

    Adicionando novos arquivos no javaScript

---
+++ javaScript/func.js
@@ -0,0 +1,15 @@
+function mostrarNome(nome) {
+   document.write(nome);
+}
+
+function pularLinha(){
+    document.write("<br><br>");
+}
+//document.write (para colocar algo na tela)
+//document.write("Stefany");
+
+//chamar função
+pularLinha();
+mostrarNome("Rodrigo");
+pularLinha();
+mostrarNome("Stefany");
