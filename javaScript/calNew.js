57507b23e02893fa6cf112f5b91c5f7a562e1a0e
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-03 16:13:03 -0300

    Adicionando função somar, multiplicar, dividir, subtrair

---
+++ javaScript/calNew.js
@@ -0,0 +1,40 @@
+//Duas variaveis
+
+//Funções
+function somar(num1, num2) {
+    console.log("Resultado da Soma")
+    console.log(num1 + num2);
+}
+
+//imprimir funções
+somar(10, 10);
+somar(10, 20);
+somar(10,5)
+somar(3,3)
+
+function multiplicação(num1, num2) {
+    console.log(num1 * num2);
+    console.log("Resultado da Multiplicação")
+}
+multiplicação(10, 10);
+multiplicação(10, 20);
+multiplicação(10,5);
+multiplicação(3,3);
+
+function divisão(num1, num2) {
+    console.log("Resultado da divisão")
+    console.log(num1 / num2);
+}
+divisão(10, 10);
+divisão(10, 20);
+divisão(10,5);
+divisão(3,3);
+
+function subitração(num1, num2) {
+    console.log("Resultado da Subtração")
+    console.log(num1 - num2);
+}
+subitração(10, 10);
+subitração(10, 20);
+subitração(10,5);
+subitração(3,3);
