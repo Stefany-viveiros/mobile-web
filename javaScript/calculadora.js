bc8d75d353887c7f0c93de4756be67afddbde7b9
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-04 16:21:33 -0300

    Adicionando calculadora

--- javaScript/calculadora.js
+++ javaScript/calculadora.js
@@ -1,5 +1,41 @@
 //Varivel
 const num1 = document.getElementById("numero1");
 const num2 = document.getElementById("numero2");
+const resultado = document.getElementById("resultado");
 
+//Função
+function soma(event) {
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) + Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
 
+function Sub(event) {
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) - Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
+
+function Multiplicação(event) {
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) * Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
+
+function divisao(event) {
+    event.preventDefault();
+    console.log(num1.value);
+    console.log(num2.value);
+    resultado.innerHTML = Number(num1.value) / Number(num2.value);
+    num1.value = "";
+    num2.value = "";
+}
