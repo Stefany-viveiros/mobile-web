e771db600d91e8d886489598538e39fd10c85375
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-10 16:07:37 -0300

    Adicionando if, else

--- javaScript/nota.js
+++ javaScript/nota.js
@@ -3,6 +3,17 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
 
 //Função
-function resultadoNotaClick() {
-    alert("Resultado");
+function resultadoNotaClick(event) {
+    event.preventDefault();
+    console.log(inputNota.value);
+   
+    
+    if (inputNota.value >=6) {
+       resultado.innerHTML = "Aprovado";
+    } else {
+        resultado.innerHTML = "Reprovado";
+
+    
+    inputNota.value = "";
+}
 }
