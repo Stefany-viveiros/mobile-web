e857f1d52dfc2927b2471900806775490f7eb6f7
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-10 14:17:31 -0300

    Adicionando calculadora

---
+++ javaScript/js-exemplos/tabuada.js
@@ -0,0 +1,9 @@
+let numero = 5;
+let multiplicador = 0;
+
+console.log(`Tabuada do ${numero}:`);
+
+while (multiplicador <= 10) {
+  console.log(`${numero} x ${multiplicador} = ${numero * multiplicador}`);
+  multiplicador++;
+}
