75cc2e0959cbb69690cd995f760722ff683b7c79
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-19 16:19:14 -0300

    Adicionado contador no react

--- projeto-b2/src/componentes/Contador.jsx
+++ projeto-b2/src/componentes/Contador.jsx
@@ -1,24 +1,29 @@
-import { Fragment } from "react"
+import "./contador.css";
+import { Fragment, useState } from "react"
 
 const Contador = () => {
+    //Inicializa o estado com valor 0
+    const [count, setcount] = useState(0);
+
+
     const Aumentar = () => {
-        alert("Aumentar")
+       setcount(count + 1)
     
     }
     
     const Diminuir = () => {
-        alert("Diminuir")
+        setcount(count - 1)
     }
     return(
-        <Fragment>
-            <h1>Contador</h1>
+        <main className ="container">
+            <h1>{count}</h1>
 
             <section>
                 <h2>Resultado</h2>
                 <button onClick={Aumentar} style={{marginRight: '10px'}}>Aumentar</button>
                 <button onClick={Diminuir}>Diminuir</button>
             </section>
-        </Fragment>
+        </main>
     )
 }
 
