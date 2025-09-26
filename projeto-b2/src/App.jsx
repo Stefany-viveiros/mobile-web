24d033a1ea7a99b850d5e4b75aa27bc9735659c8
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-26 15:15:44 -0300

    Adicionando imagens e texto no reat

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -1,12 +1,16 @@
-import Header from "./componentes/Header"
-import Banner from "./componentes/Banner"
-import ImgCard from "./componentes/ImgCard"
-import Button from "./componentes/Button"
-import "./App.css"
-
+import Header from "./componentes/Header";
+import Banner from "./componentes/Banner";
+import ImgCard from "./componentes/ImgCard";
+import Button from "./componentes/Button";
+import img1 from "./assets/img-card1.jpg";
+import img2 from "./assets/img-card2.jpg";
+import img3 from "./assets/img-card3.jpg";
+import "./App.css";
 
 function App() {
-  
+  const handleClick = () => {
+    alert("testes");
+  }
 
   return (
     <main className="container">
@@ -16,9 +20,25 @@ function App() {
         <p>Aqui você encontra as melhores ofertas!</p>
       </Banner>
 
-      <ImgCard caption="Uma imagem interessante"/>
+      <ImgCard 
+      caption="Uma imagem interessante"
+      imagem= {img1}
+      texto= "Popeto new"
+      />
 
-      <Button text="Clique aqui!"/>
+      <ImgCard
+      caption="Olá Popeto" 
+      imagem={img2}
+      texto="Popeto Bangu"
+      />
+
+      <ImgCard
+      caption="Caio do Futebol" 
+      imagem={img3}
+      texto="Popeto 3"/>
+
+
+      <Button text="Clique aqui!" onclick={handleClick}/>
     </main>
     
     
