fcd67d2cecaa05b94aaa293f5e2cba0e924af76a
stefany-viveiros <viveirosstefany@gmail.com>
2025-09-30 16:23:52 -0300

    Adicionando lista de cursos

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,55 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Prime from './src/Telas/Prime';
 
 export default function App() {
   return (
     <>
-    <View style={estilos.topo}>
-      <Text style={estilos.tituloHeader}>Stefany Barbosa</Text>
-      <Text style={estilos.corpo}>Bem vindo!!!</Text>
-    </View>
-
     <View>
-      <Text style={estilos.popeto}>Popeto</Text>
+      <Prime />
     </View>
   </>
 
   );
 }
 
-const estilos = StyleSheet.create({
- topo:{
-  width:"100%",
-  height:150,
-  backgroundColor:"purple",
- },
-
- tituloHeader:{
-  width:"100%",
-  color:"white",
-  textAlign:"center",
-  paddingTop:60,
-  fontSize:20,
-
- },
-
- corpo:{
-  width:"100%",
-  height:300,
-  color:"black",
-  textAlign:"center",
-  paddingTop:60,
-  fontSize:20,
-  backgroundColor:"gray",
-
- },
- popeto:{
-  width:"100%",
-  height:300,
-  color:"black",
-  textAlign:"center",
-  paddingTop:60,
-  fontSize:20,
-  backgroundColor:"purple",
-
- }
-});
--- meuapp/src/Telas/Prime.js
+++ meuapp/src/Telas/Prime.js
@@ -1,8 +1,53 @@
 import {View, StyleSheet,Text} from "react-native";
 export default function Prime() {
   return (
-    <View>
-        <Text>B1 Dev - Popeto</Text>
-    </View>
-  )
+     <>
+        <View style={estilos.topo}>
+          <Text style={estilos.tituloHeader}>Cursos SENAI</Text>
+        </View>
+    
+        <View>
+          <Text style={estilos.cursosContent}>HTML Completo</Text>
+          <Text style={estilos.cursosContent}>Programação de Internet</Text>
+          <Text style={estilos.cursosContent}>Informática Básica</Text>
+          <Text style={estilos.cursosContent}>Programação em Python</Text>
+          <Text style={estilos.cursosContent}>Excel Avançado</Text>
+        </View>
+      </>
+    
+    
+  );
 }
+
+const estilos = StyleSheet.create({
+ topo:{
+  width:"100%",
+  height:150,
+  backgroundColor:"purple",
+  borderRadius:20
+  
+ },
+
+ tituloHeader:{
+  width:"100%",
+  color:"white",
+  textAlign:"center",
+  paddingTop:60,
+  fontSize:20,
+
+
+ },
+    cursosContent:{
+    width:"100%",
+    textAlign:"center",
+    backgroundColor:"gray",
+    padding:15,
+    fontSize:18,
+    
+    
+
+    
+    
+
+ }
+});
