006b675cc4f7daf3f0bcbc3aa4c515e414b3e7c6
stefany-viveiros <viveirosstefany@gmail.com>
2025-10-17 16:15:02 -0300

    Adicionando Lista contatos

--- meuapp/src/Telas/ListaContatos.js
+++ meuapp/src/Telas/ListaContatos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import axios from 'axios';
 
 export default function ListaContatos() {
@@ -8,7 +8,7 @@ export default function ListaContatos() {
     // Funçaõ para buscar contatos do servidor
     const listaContatos = () => {
         axios
-            .get("http://10.0.2.2:3000/contatos")
+             .get("http://10.0.2.2:3000/contatos")
             .then((resposta) => {
                 setContatos(resposta.data)
             })
@@ -24,7 +24,60 @@ export default function ListaContatos() {
 
     return (
         <View>
-            <Text>Lista Contatos</Text>
+            <Text style={estilos.contatos}>Lista de Contatos</Text>
+            {contatos.length > 0 ? (
+                contatos.map((contato, index) => (
+                    <View style={estilos.topo} key={index}>
+                    <Text style={estilos.nome}>Nome: {contato.nome}</Text>
+                    <Text style={estilos.nome}> Tel: {contato.telefone}</Text>
+                    </View>
+                ))
+
+            ) : (
+                <Text> Nenhum contato disponivel </Text>
+            )}
         </View>
     )
 }
+
+const estilos = StyleSheet.create({
+    nome:{
+        width:"100%",
+        textAlign:"center",
+        padding:20,
+        backgroundColor:"#D7D7DE",
+    
+        
+       
+    },
+    contatos:{
+        width:"100%",
+        fontSize:20,
+        textAlign:"center",
+        padding:20,
+        backgroundColor:"gray",
+        borderBottomColor: "#1e348aff",
+        borderBottomWidth:3,
+
+
+    },
+    topo:{
+        marginBottom:8,
+        padding:10,
+        textAlign:"center",
+        fontSize:20,
+        marginRight:50,
+        marginLeft:50,
+        marginTop:10,
+        fontWeight:"bold"
+        
+        
+    
+      
+        
+    }
+
+})
+    
+
+   
